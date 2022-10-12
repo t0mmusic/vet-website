a84422728799b42bf1a2200bfb0a965b10df19c1
Jordan Brown <jebrown2091@gmail.com>
2022-10-12 16:57:15 +1030

    lotsa changes

---
+++ scripts/animal.js
@@ -0,0 +1,37 @@
+profPic = localStorage.getItem("profilePic");
+chooseAnimal(profPic);
+
+function chooseAnimal(input) {
+    animalImg = document.getElementById('profileImage');
+    formButton = document.getElementById('dogLvrsOnly');
+    console.log("Value of input: " + input);
+    if (input == "null")
+    {
+        document.getElementById('firstVisit').style.display = "flex";
+        return ;
+    }
+    document.getElementById('firstVisit').style.display = "none";
+    console.log(document.getElementById('firstVisit').style.display);
+    if (!animalImg)
+    {
+        console.log("animalImg: " + animalImg);
+        return ;
+    }
+    if (input == "cat")
+    {
+        animalImg.src="imgs/cat-pic.webp";
+        formButton.style.display = 'none';
+        document.getElementById("popupForm").style.display = "none";
+    }
+    if (input == "dog")
+    {
+        animalImg.src="imgs/dog-pic.jpg";
+        formButton.style.display = 'flex';
+    }
+    if (input == "both")
+    {
+        animalImg.src="imgs/cat-dog-pic.webp";
+        formButton.style.display = 'flex';
+    }
+    localStorage.setItem("profilePic", input);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,3 @@
-profPic = localStorage.getItem("profilePic");
-chooseAnimal(profPic);
-
-function chooseAnimal(input) {
-    animalImg = document.getElementById('profileImage');
-    formButton = document.getElementById('dogLvrsOnly');
-    if (input == "cat")
-    {
-        animalImg.src="imgs/cat-pic.webp";
-        formButton.style.display = 'none';
-        document.getElementById("popupForm").style.display = "none";
-    }
-    if (input == "dog")
-    {
-        animalImg.src="imgs/dog-pic.jpg";
-        formButton.style.display = 'flex';
-    }
-    if (input == "both")
-    {
-        animalImg.src="imgs/cat-dog-pic.webp";
-        formButton.style.display = 'flex';
-    }
-    localStorage.setItem("profilePic", input);
-}
 
 function openForm() {
     closeSignUp();
